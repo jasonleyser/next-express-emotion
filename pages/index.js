61b8cde33ef847eb76214126734dde6720a64b52
jasonleyser <jason.leyser@protocol.ai>
2020-11-21 11:29:45 -0700

    Added songs and updates urls

--- pages/index.js
+++ pages/index.js
@@ -47,11 +47,132 @@ const STYLES_LAYOUT = css`
 `;
 
 export default class IndexPage extends React.Component {
+<<<<<<< Updated upstream
   render() {
     const title = "next-express-emotion";
     const description =
       "minimal example for a full client server web application with next, express, and emotion.";
     const url = "https://github.com/jimmylee/next-express-emotion";
+=======
+  songDataInit = data[Math.floor(Math.random() * data.length)];
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      started: true,
+      playing: true,
+      song_url: this.songDataInit.hash,
+      name: this.songDataInit.name,
+      artist: this.songDataInit.artist,
+      year: this.songDataInit.year,
+      discogs: null,
+      inline: true,
+      played: null,
+      end: null,
+      runtime: 0,
+      transition: false,
+      level: 0,
+    };
+
+    console.log(this.state);
+  }
+
+  handlePlayPause = () => {
+    console.log("Play/pause");
+    this.setState({
+      playing: !this.state.playing,
+    });
+  };
+
+  handleEnded = () => {
+    console.log("The song has ended");
+
+    //const lvl = this.state.level;
+
+    //if(lvl === 3){
+    //lvl === 1;
+    //console.log(lvl);
+    //}else{
+    //lvl = lvl + 1;
+    //console.log(lvl);
+    //}
+
+    const next = data[Math.floor(Math.random() * data.length)];
+
+    this.setState({
+      song_url: next.hash,
+      name: next.name,
+      artist: next.artist,
+      discogs: "https://www.discogs.com/" + next.discogs,
+      year: next.year,
+      end: next.end,
+      playing: true,
+      transition: false,
+    });
+    console.log(this.state);
+  };
+
+  handlePlay = () => {
+    console.log("Start playing");
+    this.setState({ playing: true });
+  };
+
+  handleProgress = (state) => {
+    console.log({ state }, state);
+    this.setState({ state });
+
+    console.log(this.state.end);
+    console.log(state.playedSeconds);
+
+    var rnt = this.state.runtime;
+
+    this.setState({ runtime: rnt + 1 });
+
+    if (state.playedSeconds > this.state.end) {
+      console.log("Last 10 remaining");
+
+      if (this.state.transition === false) {
+        this.setState({ transition: true });
+
+        //var transitionSound = new Howl({
+        //  src: ["/public/static/horn.wav"],
+        //  volume: 0.75,
+        //  rate: 0.75,
+        //});
+
+        //const x = Math.round(100 * (2.5 - Math.random() * 5)) / 100;
+        //const y = Math.round(100 * (2.5 - Math.random() * 5)) / 100;
+
+        //transitionSound.pos(x, y);
+        //transitionSound.play();
+      }
+    } else {
+      console.log("More than 10 remaining");
+      this.setState({ transition: false });
+    }
+  };
+
+  ref = (player) => {
+    this.player = player;
+  };
+
+  render() {
+    const title = "Welcome to midnight school";
+    const description = "Where underground 90s hiphop happens";
+    const url = "https://midnight.school/";
+    const {
+      song_url,
+      playing,
+      name,
+      artist,
+      year,
+      discogs,
+      inline,
+      played,
+      runtime,
+    } = this.state;
+>>>>>>> Stashed changes
 
     return (
       <React.Fragment>
